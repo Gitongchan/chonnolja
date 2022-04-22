c777e3ce9d763d6a66cc25a712c96898af28fabb
Jinny <ppwm111@naver.com>
2022-04-22 11:08:21 +0900

    css 색상 변경 및 프로젝트 설정 변경

---
+++ src/main/java/com/chonnolja/opendataservice/user/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.chonnolja.opendataservice.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required = false)String error,
+                        @RequestParam(value = "exception", required = false)String exception,
+                        Model model) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
+        return "/pages/login";
+    }
+
+}
---
+++ src/main/java/com/chonnolja/opendataservice/user/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.chonnolja.opendataservice.user.repository;
+
+import com.chonnolja.opendataservice.user.model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserInfo,Integer> {
+    Optional<UserInfo> findByUserid(String userid);
+
+    Optional<UserInfo> findByEmail(String email);
+
+    Optional<UserInfo> findById(Long id);
+
+    Optional<UserInfo> findByCompanyName(String companyName);
+}
